31d93655158dd5f5da3d5c7eb74cbaa20ac5df2f
faharetana308 <andriamanana083faharetana@gmail.com>
2025-07-18 09:16:59 +0200

    correction de l'erreur lors de la conexion au BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { Appartement } from './appartement/appartement.entity';
 
 import { ImmeubleModule } from './immeuble/immeuble.module';
 import { AppartementModule } from './appartement/appartement.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
   imports: [
@@ -17,9 +19,9 @@ import { AppartementModule } from './appartement/appartement.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT ?? '5050', 10),
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
       username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
+      password: String(process.env.DB_PASSWORD),
       database: process.env.DB_NAME,
       entities: [Immeuble, Appartement],
       synchronize: true, // attention à mettre false en production
